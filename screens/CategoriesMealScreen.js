9ac3913f8c76efa308e1f13afa6cbd33d3df17c6
sgolrang <sanagolrang@gmail.com>
2021-01-05 10:08:52 -0500

    Dynamic navigation Options

--- screens/CategoriesMealScreen.js
+++ screens/CategoriesMealScreen.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Text, View, StyleSheet, Button, ShadowPropTypesIOS } from 'react-native';
 import {CATEGORIES} from '../data/dummy-data';
-
+import Colors from '../constants/Colors';
 
 const CategoriesMealScreen = props => {
     // to access a param we defined in categoriesScreen.js, we have getParams: a method provided to extract a parameter
     // we receive. getParam takes a string with the name of the parameter we want to extract( the name is what you chose as a key)
-    const catId = props.navigation.getParam('categoryId');
+     const catId = props.navigation.getParam('categoryId');
 
     // This will takes CATEGORIES and we execute find on eact element of it and gives us the item where this function 
     // returns true which should be the case of the id for the category we'are looking at matches the cat Id we're
     // retrieving from our params
-    const selectedCategory = CATEGORIES.find(cat=> cat.id === catId );
+     const selectedCategory = CATEGORIES.find(cat=> cat.id === catId );
     return (
         <View style={styles.headline}>
             <Text>The Category Meal Screen</Text>
@@ -25,6 +25,25 @@ const CategoriesMealScreen = props => {
         </View>
     )
 };
+// To set the navigation header, option #2: "Setting Dynamic Navigation Options": why to use dynamic? because here 
+// selectedCategory is inside the function and navigation happens after the function definition, so in order to set 
+// dynamically, we pass "naviationData"
+CategoriesMealScreen.navigationOptions = (navigationData) => {
+    // Then, to have access to the data that is previously used in the function, categoryId and selectedCategory, again
+    // we define them.
+const catId = navigationData.navigation.getParam('categoryId');
+const selectedCategory = CATEGORIES.find( cat=> cat.id === catId );
+// 
+return {
+    // This is how we can dynamically derive the header
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+        backgroundColor: Colors.primaryColor
+    },
+    headerTintColor: 'white'
+}
+}
+
 const styles = StyleSheet.create({
     headline: {
         textAlign: 'center',
