837cb5e93e900e79ea44111d4552d062fe0c8849
sgolrang <sanagolrang@gmail.com>
2021-01-06 10:59:28 -0500

    Loading meals for categories

--- screens/CategoriesMealScreen.js
+++ screens/CategoriesMealScreen.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button, ShadowPropTypesIOS } from 'react-native';
-import {CATEGORIES} from '../data/dummy-data';
+import { Text, View, StyleSheet, FlatList, ShadowPropTypesIOS } from 'react-native';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
 import Colors from '../constants/Colors';
+import { TextInput } from 'react-native-gesture-handler';
 
 const CategoriesMealScreen = props => {
-    // to access a param we defined in categoriesScreen.js, we have getParams: a method provided to extract a parameter
-    // we receive. getParam takes a string with the name of the parameter we want to extract( the name is what you chose as a key)
+   /*   to access a param we defined in categoriesScreen.js, we have getParams: a method provided 
+   to extract a parameter we receive. 
+   getParam takes a string with the name of the parameter we want to extract( the name is what you chose as a key) */
      const catId = props.navigation.getParam('categoryId');
+/* 
+     This will takes CATEGORIES and we execute find on eact element of it and gives us the item where this function 
+     returns true which should be the case of the id for the category we'are looking at matches the cat Id we're
+     retrieving from our params */
 
-    // This will takes CATEGORIES and we execute find on eact element of it and gives us the item where this function 
-    // returns true which should be the case of the id for the category we'are looking at matches the cat Id we're
-    // retrieving from our params
-     const selectedCategory = CATEGORIES.find(cat=> cat.id === catId );
+    //  const selectedCategory = CATEGORIES.find(cat=> cat.id === catId );
+
+    const renderMealItem = itemData => {
+        return (
+            <View>
+                <Text>{itemData.item.title}</Text>
+            </View>
+        )
+    }
+
+    /* we want to find the meals that belong to the category we selected: if it's 0 or bigger than 0 then we know 
+    it is available in MEALS */
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0 
+    );
+
     return (
         <View style={styles.headline}>
-            <Text>The Category Meal Screen</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="Go to Meal Details" onPress={() => {
-                props.navigation.navigate('MealDetail')
-            }} />
-            <Button title="Go Back" onPress={() => {
-                props.navigation.goBack();
-            }} />
+            <FlatList data={displayedMeals}
+            /* Modern version of react native does not need the keyExtractor, it does it automatically but just in case: */ 
+            keyExtractor={(item, index) => item.id}
+            renderItem= {renderMealItem}
+            />
+           
+    
         </View>
     )
 };
